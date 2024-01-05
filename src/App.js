2d8516da1a9e12d1eb9d1dd5ef09961bfa316878
Mahammad Shahid <shahid130820@gmail.com>
2024-01-05 20:06:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import UserCards from './components/UserCards';
+import EditUser from './components/EditUser';
+import { Routes, Route, useNavigate } from "react-router-dom"
+import AddUser from './components/AddUser';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { useState} from 'react';
+
 
 function App() {
+
+  const navigate = useNavigate()
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div className="App">  
+    <h1 style={{color:"coral"}}><i>Axios Users</i></h1>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab  onClick={() => navigate("/")} label="Users" value="1" />
+            <Tab onClick={() => navigate("/adduser")} label="Add Users" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel  value="1"></TabPanel>
+        <TabPanel  value="2"></TabPanel>
+      </TabContext>
+    </Box>
+
+
+         <Routes>
+         <Route path="/" element={<UserCards />} />
+         <Route path="/users/edit/:userid" element={<EditUser/>} />
+         <Route path="/adduser" element={<AddUser/>} />
+      </Routes>
+     
     </div>
   );
 }
